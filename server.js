09f435a560c1d9bb1611a53d560c2151527dd96a
Rahul Kailash Yadav <rahuly1903@gmail.com>
2021-10-06 16:29:17 +0530

    added altab connection string link using env variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://USERNAME:PASSWORD@CLUSTER_NAME.mongodb.net/DATABASE_NAME?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 
 // use the express-static middleware
 app.use(express.static("public"));
@@ -12,39 +12,37 @@ app.use(express.static("public"));
 // define the first route
 app.get("/api/movie", async function (req, res) {
   const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
+
   try {
     await client.connect();
 
-    const database = client.db('sample_mflix');
-    const collection = database.collection('movies');
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
 
     // Query for a movie that has the title 'Back to the Future'
     const query = { genres: "Comedy", poster: { $exists: true } };
     const cursor = await collection.aggregate([
       { $match: query },
       { $sample: { size: 1 } },
-      { $project: 
-        {
+      {
+        $project: {
           title: 1,
           fullplot: 1,
-          poster: 1
-        }
-      }
+          poster: 1,
+        },
+      },
     ]);
 
     const movie = await cursor.next();
 
     return res.json(movie);
-  } catch(err) {
+  } catch (err) {
     console.log(err);
-  }
-  finally {
+  } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 });
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
